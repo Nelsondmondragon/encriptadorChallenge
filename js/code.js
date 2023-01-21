39f7db2b2e8070cfdc4458538ce4cd4bb43f498d
Nelson M <nelsondmondragon@gmail.com>
2023-01-20 23:31:13 -0500

    layout table and desktop

---
+++ js/code.js
@@ -0,0 +1,16 @@
+
+function encryptText() {
+    let textInput = document.querySelector(".area").value;
+    textInput = textInput.replaceAll("e", "enter");
+    textInput = textInput.replaceAll("i", "imes");
+    textInput = textInput.replaceAll("a", "ai");
+    textInput = textInput.replaceAll("o", "ober");
+    textInput = textInput.replaceAll("u", "ufat");
+    console.log(textInput);
+}
+
+
+function decryp() {
+    let textOutput = document.querySelector("")
+
+}
